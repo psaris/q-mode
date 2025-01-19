30a88340e72cda4f033a7e5f0e3a5aae8a1b195c
Gchouchou <45127928+Gchouchou@users.noreply.github.com>
2025-01-19 10:12:50 -0500

    change `q-constant-keywords` regex-opt from words to symbols (#10)

--- q-mode.el
+++ q-mode.el
@@ -550,7 +550,7 @@ This marks the PROCESS with a MESSAGE, at a particular time point."
     (regexp-opt '(".z.D" ".z.K" ".z.T" ".z.Z" ".z.N" ".z.P" ".z.a" ".z.b" ".z.d" ".z.exit" ".z.f"
                   ".z.h" ".z.i" ".z.k" ".z.l" ".z.o" ".z.pc" ".z.pg" ".z.ph" ".z.pi"
                   ".z.po" ".z.pp" ".z.ps" ".z.pw" ".z.s" ".z.t" ".z.ts" ".z.u" ".z.vs"
-                  ".z.w" ".z.x" ".z.z" ".z.n" ".z.p" ".z.ws" ".z.bm") `words))
+                  ".z.w" ".z.x" ".z.z" ".z.n" ".z.p" ".z.ws" ".z.bm") `symbols))
   "Constants for q mode.")
 
 (defvar q-font-lock-keywords
